84a58fb4f7fdc7f1c106d076988541227afe6a8d
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-01-15 20:51:24 -0300

    binding test

--- checklista/src/app/app.component.ts
+++ checklista/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CategoryComponent } from "./category/category.component";
 import { TestComponent } from "./test/test.component";
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CategoryComponent, TestComponent],
+  imports: [RouterOutlet, CategoryComponent, TestComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- checklista/src/app/test/test.component.ts
+++ checklista/src/app/test/test.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
-export class TestComponent {
+export class TestComponent implements OnInit{
 
   public stringInterpolation = 'Este é um teste';
 
   public isDisabled = false;
+
+  public myBindingProperty: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
   public disabledButton(){
     if(this.isDisabled) this.isDisabled = false;
