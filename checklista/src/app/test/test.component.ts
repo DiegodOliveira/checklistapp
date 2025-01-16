14b39589db7a714822fd1aae1486ee20cc4e11a9
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-01-15 21:31:33 -0300

    ngFor with problems

--- checklista/src/app/test/test.component.ts
+++ checklista/src/app/test/test.component.ts
@@ -10,19 +10,33 @@ import {FormsModule} from '@angular/forms'
 })
 export class TestComponent implements OnInit{
 
-  public stringInterpolation = 'Este é um teste';
+  // public stringInterpolation = 'Este é um teste';
 
-  public isDisabled = false;
+  // public isDisabled = false;
 
-  public myBindingProperty: string = '';
+  // public myBindingProperty: string = '';
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  public disabledButton(){
-    if(this.isDisabled) this.isDisabled = false;
-    else this.isDisabled = true;
+  // public disabledButton(){
+  //   if(this.isDisabled) this.isDisabled = false;
+  //   else this.isDisabled = true;
+  // }
+
+  public showTitle = true;
+
+  public changeTitleDisplayStatus(){
+    if(this.showTitle) this.showTitle = false;
+    else this.showTitle = true;
   }
+
+  public items = [
+    { description:  "item 1", id: 1},
+    { description:  "item 2", id: 2},
+    { description:  "item 3", id: 3},
+    { description:  "item 4", id: 4}
+  ];
 
 }
