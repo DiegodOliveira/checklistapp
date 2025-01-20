efcd6018547f7aa572f477ab9bf287371744a8c0
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-01-20 18:39:09 -0300

    nav incomplete

--- checklista/src/app/app.component.ts
+++ checklista/src/app/app.component.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CategoryComponent } from "./category/category.component";
 import { TestComponent } from "./test/test.component";
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CategoryComponent, TestComponent, FormsModule, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, CategoryComponent, TestComponent, FormsModule, HeaderComponent, FooterComponent, MatSidenavModule, MatListModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'checklista';
+
+  public events: string[] = [];
+  public opened: boolean = false;
+
 }
