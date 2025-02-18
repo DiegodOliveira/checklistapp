21b96a475059736c3f76237a277665ca6459d93c
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-02-18 17:44:14 -0300

    forms corrigido junto com botões

--- checklista/src/app/category-form/category-form.component.ts
+++ checklista/src/app/category-form/category-form.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 
 
 @Component({
   selector: 'app-category-form',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './category-form.component.html',
   styleUrl: './category-form.component.css'
 })
