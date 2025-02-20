bdeb6486d12460646ce1f54ac5174a5a46997a2e
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-02-20 10:56:13 -0300

    Forms done

--- checklista/src/app/category-form/category-form.component.ts
+++ checklista/src/app/category-form/category-form.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card'
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+// import { MatCardModule } from '@angular/material/card'
+// import { MatButtonModule } from '@angular/material/button';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { MatInputModule } from '@angular/material/input';
 
-
+import { MaterialModule } from "../material.model";
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
   selector: 'app-category-form',
-  imports: [MatCardModule, MatButtonModule, FormsModule, ReactiveFormsModule],
+  imports: [MaterialModule],
   templateUrl: './category-form.component.html',
   styleUrl: './category-form.component.css'
 })
-export class CategoryFormComponent {
-categoryForm: any;
+export class CategoryFormComponent implements OnInit {
+
+  public actionName = 'Editar'
+  public categoryForm: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder){
+
+    this.categoryForm = this.formBuilder.group({
+      name: 'Diego',
+      age: '21',
+      profession: 'Programador'
+    });
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  public cancel(){
+    console.log('Cancelar clicado');
+  }
+
+  public save(){
+    console.log('salvar clicado');
+    
+  }
 
 }
