e06c400bcd5d8c1b5706f11226fc70b26641a7cb
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-02-24 22:25:15 -0300

    checklist router

--- checklista/src/app/app.routes.ts
+++ checklista/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CategoryComponent } from './category/category.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ChecklistComponent } from './checklist/checklist.component';
 
 export const routes: Routes = [
      {path: '', component: HomeComponent},
      {path: 'categoria', component: CategoryComponent},
-     { path: '**', component: PageNotFoundComponent}
+     { path: '**', component: PageNotFoundComponent},
+     {path: 'checklist', component: ChecklistComponent}
 ];
