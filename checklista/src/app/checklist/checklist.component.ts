ad9cb9c6e3fd896235bbcbaa477128ddca8e360f
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2025-02-27 19:59:39 -0300

    checklist completed

--- checklista/src/app/checklist/checklist.component.ts
+++ checklista/src/app/checklist/checklist.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../material.model';
 import { RouterLink } from '@angular/router';
 import { CATEGORY_DATA } from '../category/category.component';
+import { ChecklistItem } from '../_models/checklist_item';
+import { DialogComponent } from '../dialog/dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 export const CHECKLIST_DATA = [
 
@@ -22,14 +26,50 @@ export class ChecklistComponent implements OnInit {
 
   public displayedColumns: string[] = ['id','completed', 'description', 'deadline', 'postDate','category', 'actions'];
 
-  constructor(){}
+  constructor(private dialog: MatDialog){}
 
   ngOnInit(): void{
 
   }
 
+  public updateCompleteStatus(status: boolean){
+    console.log(`status alterado ${status}`)
+  }
+
+
+  public updateChecklistItem(checklistItem: ChecklistItem){
+    console.log("atualizando item da checklist");
+
+    this.dialog.open(DialogComponent, {
+      disableClose: true, data: {actionName: 'Criar'},
+    }).afterClosed().subscribe( resp => {
+      console.log('Fechando modal de update');
+      
+    })
+    
+  }
+
+  public deleteChecklistItem(checklistItem: ChecklistItem){
+    console.log("atualizando item da checklist");
+    
+    this.dialog.open(DialogComponent, { disableClose: true, data: {
+      msg: 'Você deseja realmente apagar esse item?', leftButtonLabel: 'Cancelar', rightButtonLabel: 'Ok'
+    }}).afterClosed().subscribe(resp => {
+      console.log('Janela modal confirmar apagar fechada');
+      
+    })
+
+  }
+
   public createNewItem(){
     console.log('Criar novo item do checklist clicado');
+
+    this.dialog.open(DialogComponent, {
+      disableClose: true, data: {actionName: 'Criar'},
+    }).afterClosed().subscribe( resp => {
+      console.log('Fechando modal de criação');
+      
+    })
   }
 
 }
