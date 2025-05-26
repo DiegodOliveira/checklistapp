e01e993282a6181e1ccae1c5c1a43b46e35d8f66
DiegodOliveira <ciprianodiegooliveira@gmail.com>
2025-05-26 17:31:18 -0300

    app module dando errado

--- checklista/src/app/app.module.ts
+++ checklista/src/app/app.module.ts
@@ -1,46 +1,55 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component.ts';
 import { MaterialModule } from './material.model';
-import { CategoryComponent } from "./category/category.component";
-import { MatIconModule } from "@angular/material/icon";
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { RouterLink } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CategoryComponent } from './category/category.component';
+import { DialogComponent } from './dialog/dialog.component';
 import { CategoryEditComponent } from './category-edit/category-edit.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { ChecklistComponent } from './checklist/checklist.component';
+import { ChecklistEditComponent } from './checklist-edit/checklist-edit.component';
+import { ChecklistFormComponent } from './checklist-form/checklist-form.component';
 
-
-import localePt from '@angular/common/locales/pt'
+//configuring pt-BR locale
+import { LOCALE_ID, NgModule } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 registerLocaleData(localePt)
 
-
-
 @NgModule({
-  declarations: [],
+declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    CategoryComponent,
+    DialogComponent,
+    CategoryEditComponent,
+    CategoryFormComponent,
+    ChecklistComponent,
+    ChecklistEditComponent,
+    ChecklistFormComponent
+  ],
   imports: [
-    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
     FormsModule,
-    MaterialModule
-    , HeaderComponent
-    , FooterComponent
-    , MatSidenavModule
-    , MatListModule
-    , MatIconModule
-    , CategoryComponent
-    , HomeComponent
-    , RouterLink
-    , ReactiveFormsModule
-    , CategoryEditComponent
-    , CategoryFormComponent
-    , NgModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [
-    { provide:  LOCALE_ID, useValue: 'pt-BR' }
-  ]
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- checklista/src/app/material.model.ts
+++ checklista/src/app/material.model.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox'
-import { MatDividerModule } from '@angular/material/divider';
-import { MatCardModule } from '@angular/material/card'
-import { MatTableModule } from '@angular/material/table'
-import { MatDialogModule } from '@angular/material/dialog'
-import { MatInputModule } from '@angular/material/input'
-import { MatSelectModule } from '@angular/material/select';
-import { MatDatepickerModule } from '@angular/material/datepicker'
-import { MatNativeDateModule } from '@angular/material/core';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatNativeDateModule} from '@angular/material/core';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-
 
 @NgModule({
   imports: [
@@ -25,38 +22,33 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    FormsModule,
-    CommonModule,
-    MatCheckboxModule,
     MatDividerModule,
     MatCardModule,
     MatTableModule,
     MatDialogModule,
     MatInputModule,
-    ReactiveFormsModule,
     MatSelectModule,
     MatDatepickerModule,
+    MatCheckboxModule,
     MatNativeDateModule,
     MatSnackBarModule
   ],
-  exports:[
+  exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    FormsModule,
     MatSidenavModule,
     MatListModule,
-    CommonModule,
-    MatCheckboxModule,
+    MatDividerModule,
     MatCardModule,
     MatTableModule,
     MatDialogModule,
     MatInputModule,
-    ReactiveFormsModule,
     MatSelectModule,
     MatDatepickerModule,
+    MatCheckboxModule,
     MatNativeDateModule,
     MatSnackBarModule
   ]
 })
-export class MaterialModule {}
+export class MaterialModule { }
